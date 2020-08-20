3d6a71f0ec9ec5d1024a80691983183a67dd7684
jeppeman <jesperaamann@gmail.com>
2020-08-20 23:03:35 +0200

    Linux compatible compute script

--- globallydynamic-website/frontend/public/scripts/compute_engine_launch.sh
+++ globallydynamic-website/frontend/public/scripts/compute_engine_launch.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Create a bucket in Google Cloud Storage where bundles will be stored
-bucket_id="globallydynamic-$(uuidgen)"
+bucket_id="globallydynamic-$(uuidgen | tr '[:upper:]' '[:lower:]')"
 gsutil mb -l eu "gs://${bucket_id}"
 
 port=8080
@@ -21,14 +21,16 @@ export GLOBALLY_DYNAMIC_VALIDATE_SIGNATURE_ON_DOWNLOAD=true
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 " > vm_environment
 
-# Define startup script (called when VM starts), this start the GloballyDynamic Server
+# Define startup script (called when VM starts), this will start the GloballyDynamic Server
 echo "
 #!/usr/bin/env bash
+
 set -e
 set -o xtrace
 
 # Set up environment (load GloballyDynamic Server configuration)
 curl -f http://metadata.google.internal/computeMetadata/v1/instance/attributes/environment -H \"Metadata-Flavor: Google\" > env_file
+
 source env_file
 
 # Run the server
@@ -37,16 +39,18 @@ java -jar globallydynamic-server.jar &
 
 # Create a VM instance
 gcloud compute instances create ${instance_name} \
+    --image-project=debian-cloud \
+    --image-family=debian-9 \
     --tags=http-server,https-server \
     --scopes=cloud-platform \
     --zone=${zone} \
     --metadata-from-file=environment=vm_environment,startup-script=startup_script
 
 # Build an executable server jar in the VM
-gcloud compute ssh --zone ${zone} ${instance_name} -- "sudo apt-get install -y git openjdk-8-jdk \\
-    && cd / \\
-    && sudo git clone https://github.com/jeppeman/GloballyDynamic.git \\
-    && cd GloballyDynamic/globallydynamic-server-lib \\
+gcloud compute ssh --ssh-flag="-ttn" --zone ${zone} ${instance_name} -- "sudo apt-get install -y git openjdk-8-jdk \
+    && cd / \
+    && sudo git clone https://github.com/jeppeman/GloballyDynamic.git \
+    && cd GloballyDynamic/globallydynamic-server-lib \
     && sudo ./gradlew executableJar -PoutputDir=/ -ParchiveName=globallydynamic-server.jar
 "
 
@@ -58,19 +62,18 @@ gcloud compute firewall-rules create allow-http-${port} \
     --description "Allow port ${port} access to http-server"
 
 # Restart instance
-gcloud compute instances stop ${instance_name}
-gcloud compute instances start ${instance_name}
+gcloud compute instances stop ${instance_name} --zone ${zone}
+gcloud compute instances start ${instance_name} --zone ${zone}
 
 # Cleanup
 rm startup_script
 rm vm_environment
 
 # Get the ip of the instance
-server_ip=$(gcloud compute instances describe ${instance_name} --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+server_ip=$(gcloud compute instances describe ${instance_name} --zone ${zone} --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
 server_url="http://${server_ip}:${port}"
 
 echo "Waiting for the server to start, this may take a few minutes.."
-
 iterations=0
 while [[ -z $(curl -s --max-time 2 ${server_url}) ]];
 do
