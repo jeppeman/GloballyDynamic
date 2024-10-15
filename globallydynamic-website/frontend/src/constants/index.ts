57d1870b05bb808eaec378d8d772b43a102fc35f
jeppeman <jesperaamann@gmail.com>
2024-10-15 18:46:50 +0100

    Update website

--- globallydynamic-website/frontend/src/constants/index.ts
+++ globallydynamic-website/frontend/src/constants/index.ts
@@ -19,10 +19,10 @@ const pageNames: { [key: string]: string[] } = {
 export type Versions = { AGP: string, GRADLE: string, ANDROID: string, SERVER: string, STUDIO: string }
 
 const versions: Versions = {
-    'GRADLE': '1.9.0',
+    'GRADLE': '1.10.0',
     'ANDROID': '1.3.0',
     'SERVER': '1.5.0',
-    'STUDIO': '1.11.0',
+    'STUDIO': '1.12.0',
     'AGP': '8.1.1'
 }
 
